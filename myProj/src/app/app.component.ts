04e7000c56c4bc12bc7ed7f429626649fbcd2945
RafatSuraiya <rafatsuraiya94>
2019-11-25 22:42:13 +0530

    change Detection Example

--- myProj/src/app/app.component.ts
+++ myProj/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myProj';
+ private exampleObject:{name:string,email?:string}
+ constructor(){
+   this.exampleObject={
+     name:'rafat',
+     email:'raftasuraiya94@gmail.com'
+   }
+ }
+ chaneName(){
+   this.exampleObject.name="Imran";
+ }
+ chaneObject(){
+  this.exampleObject={
+    name:'Suraiya',
+    email:'suraiya94@gmail.com'
+  }
+ }
 }
